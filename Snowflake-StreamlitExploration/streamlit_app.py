4f8c11f8a119d947b549c5534ccc3886da1f0049
Loganathankumar <66212272+Loganathankumar@users.noreply.github.com>
2025-10-04 10:37:23 +0530

    Update streamlit_app.py

--- Snowflake-StreamlitExploration/streamlit_app.py
+++ Snowflake-StreamlitExploration/streamlit_app.py
@@ -26,6 +26,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
+      st.subheader(fruit_chosen + 'Nutrituion Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
       st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
